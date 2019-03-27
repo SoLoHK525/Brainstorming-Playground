d1614b9e34734fc44cb4a1ce9ce75ea65d6e8f3e
SoLo <dickylau2013@gmail.com>
2019-03-27 20:01:10 +0800

    Create prime.js

---
+++ prime.js
@@ -0,0 +1,26 @@
+function genPrimeNum(){
+	while(true){
+		var rand = Math.round(Math.random() * 1000);
+		if(isPrime(rand)){
+			return rand;
+        }
+    }
+}
+
+function isPrime(num){
+	if(num < 2){
+		return false;
+    }else if(num == 2){
+		return true;
+    }else if(num % 2 == 0){
+		return false;
+    }
+
+	var sqr = Math.round(Math.sqrt(num));
+	for(var i = 2; i <= sqr; i+=2){
+		if(num % i == 0){
+			return false;
+        }
+    }
+	return true;
+}
