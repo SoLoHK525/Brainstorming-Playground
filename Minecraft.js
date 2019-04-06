56304ec375670c90a5b241389aaedddb001331c2
SoLo <dickylau2013@gmail.com>
2019-04-06 22:48:40 +0800

    Update Minecraft.js

--- Minecraft.js
+++ Minecraft.js
@@ -1,18 +1,7 @@
 function minecraftColor(string){
 	var output = "";
-	var color = new Array();
-	color[0] = "4";
-	color[1] = "c";
-	color[2] = "6";
-	color[3] = "e";
-	color[4] = "2";
-	color[5] = "a";
-	color[6] = "b";
-    color[7] = "3";
-	color[8] = "1";
-	color[9] = "9";
-	color[10] = "d";
-	color[11] = "5";
+	var color = ["4", "c", "6", "e", "2", "a", "b", "3", "1", "9", "d", "5"];
+
 	string = string.trim();
 	var i = 0;
 	var j = 0;
@@ -23,10 +12,10 @@ function minecraftColor(string){
 			continue;
         }
 
-		if((j != 1) && (j%11) == 1){
+		if((j != 1) && (j%color.length) == 1){
 			color.reverse();
         }
-		output += "&" + color[(j%11)] + string.charAt(i);
+		output += "&" + color[(j%color.length)] + string.charAt(i);
 		i++;
 		j++;
     }
